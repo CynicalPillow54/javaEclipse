3633c8c9ca186847cf61b91bcb82cde7531ecafe
Akhil Seeram <akhil.seeram54@gmail.com>
2025-10-28 15:22:42 +0000

    if else statemenets for masters grading

---
+++ src/Revision/ifElseMasterGrading.java
@@ -0,0 +1,55 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class ifElseMasterGrading {
+
+	public static void main(String[] args) {
+		/**
+		 * This function will:
+		 * 1. Prompt the user for a file mark (1-100)
+		 * 2. Assign the number into a suitable variable
+		 * 3. use if - else statements to output the classification
+		 * Outputs:
+		 * 0-49 = Fail
+		 * 50-59 = Pass
+		 * 60-69 = Merit
+		 * 70-100 = Distinction
+		 * 
+		 * Have error handling
+		 */
+		
+		// assign Scanner to user Input
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter mark...");
+		int userMark = keyboard.nextInt();
+		
+		if (userMark < 0 || userMark > 100) {
+			System.out.println("Pleae enter value between 0 and 100!");
+		}
+		else {
+			
+			String classification = new String();
+			
+			if (userMark >= 0 && userMark <= 49) {
+				classification = "Fail";
+			}
+			else if (userMark >= 50 && userMark <= 59) {
+				classification = "Pass";
+			}
+			else if (userMark >= 60 && userMark <= 69) {
+				classification = "Merit";
+			}
+			else if (userMark >= 70 && userMark <= 100) {
+				classification = "Distinction";
+			}
+			
+			System.out.println("Classification : " + classification);
+		}
+		
+
+		
+	}
+
+}
--- src/Revision/operatorsAndSelectionStatements.java
+++ src/Revision/operatorsAndSelectionStatements.java
@@ -22,12 +22,12 @@ public class operatorsAndSelectionStatements {
 		keyboard.close();
 		
 		//float rounded = Math.round(userInput);
-		double rounded = Math.round(userInput);
+		//double rounded = Math.round(userInput);
 		double squared = Math.pow(userInput, 2);
 		double cubed = Math.pow(userInput, 3);
 		double sqrt = Math.sqrt(userInput);
 		
-		System.out.printf("Number rounded (two decimal places)    :%.2f %n", rounded);
+		System.out.printf("Number rounded (two decimal places)    :%.2f %n", userInput);
 		System.out.printf("Number squared (three decimal places)  :%.3f %n", squared);
 		System.out.printf("Number cubed to (three decimal places) :%.3f %n", cubed);
 		System.out.printf("Square root (four decimal places) :%.4f %n", sqrt);
