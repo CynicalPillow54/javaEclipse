8482409fea8206e27792120ddcb035003119dec4
Akhil Seeram <akhil.seeram54@gmail.com>
2025-10-29 20:42:29 +0000

    loops: ASCII Converter Menu Challenge updates

--- src/loops/AsciiConverterChallenge.java
+++ src/loops/AsciiConverterChallenge.java
@@ -49,16 +49,63 @@ public class AsciiConverterChallenge {
 				switch(userChoice) {
 				case 1:
 					// for loop for printing ASCII Table
+					System.out.println();
+					System.out.println("Selected [1]: ASCII Table");
+					System.out.println("*********************");
+					System.out.println("ASCII Table ...");
 					
+					for (int i = asciiStart; i <= asciiEnd; i++) {
+						char asciiReprChar = (char) i;
+						System.out.printf("%-10d %10s%n", i, asciiReprChar);
+					}
+					
+					break;
 					
 				case 2:
 					// User input for char and cast to ASCII
+					System.out.println();
+					System.out.println("Selected [2]: Convert a Character to ASCII");
+					System.out.println("*********************");
+					System.out.print("Enter a character to see the ASCII Value: ");
+					
+					char userChar = keyboard.nextLine().charAt(0);
+					int asciiReprInt = (int) userChar;
+					
+					System.out.printf("%-10s %10d%n", userChar, asciiReprInt);
+					
+					break;
+					
 				
 				case 3:
 					// User input for string and for loop for converting each char to ASCII
+					System.out.println();
+					System.out.println("Selected [3]: Convert your name to ASCII");
+					System.out.println("*********************");
+					System.out.print("Please enter you name: ");
+					
+					String userName = keyboard.nextLine();
+					String userNameClean = userName.replace(" ", "");
+					
+					System.out.println("ASCII");
+					
+					int lengthOfName = userNameClean.length();
+					
+					for (int i = 0; i < lengthOfName; i++) {
+						char letter = userNameClean.charAt(i);
+						int asciiReprName = (int) letter;
+						System.out.printf("%-10d %10s%n", asciiReprName, letter );
+					}
+					
+					break;
 				
 				case 4:
 					// exit bool = true
+					System.out.println();
+					System.out.println("Selected [4]: Quit");
+					System.out.println("*********************");
+					System.out.println("Quitting...");
+					exit = true;
+					break;
 				}
 			}
 			else {
