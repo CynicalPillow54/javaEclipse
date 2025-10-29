ff8d2e76c823d81ad89acc5d554f91e8980b60e0
Akhil Seeram <akhil.seeram54@gmail.com>
2025-10-29 16:28:35 +0000

    loops: for loop name 10x

---
+++ src/loops/ForLoopName10x.java
@@ -0,0 +1,27 @@
+package loops;
+
+import java.util.Scanner;
+
+public class ForLoopName10x {
+
+	public static void main(String[] args) {
+		/**
+		 * Create a program that prompts the user for a name and 
+		 * prints it to screen 10 times
+		 */
+		
+		// Init Scanner 
+		Scanner keyboard = new Scanner(System.in);
+		
+		// Get users name
+		System.out.print("Please input your name: ");
+		String userName = keyboard.next();
+		
+		for (int i = 0; i < 10; i++ ) {
+			System.out.println(userName + " index: " + i);
+		}
+		
+		// Close Scanner 
+	}
+
+}
